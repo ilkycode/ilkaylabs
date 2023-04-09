98fe2b27d3c6159ca4b32a596321b960d1d9807f
ilkycode <muellerilkay@gmail.com>
2023-04-09 16:49:48 +0200

    Update week3wednesday.sql
    optional added

--- week3wednesday.sql
+++ week3wednesday.sql
@@ -52,4 +52,32 @@ GROUP BY a.first_name, a.last_name, a.email;
 #optional
 #Which are films starred by the most prolific actor? Most prolific actor is defined as the actor that has acted in the most number of films. First you will have to find the most prolific actor and then use that actor_id to find the different films that he/she starred.
 
+SELECT title FROM film WHERE film_id IN(
+SELECT film_id FROM film_actor WHERE actor_id IN(
+SELECT actor_id, COUNT(film_id) as profilic from film_actor
+GROUP BY actor_id
+ORDER BY profilic DESC
+LIMIT 1));
 
+
+SELECT title FROM film
+WHERE film_id IN (
+SELECT film_id FROM film_actor
+WHERE actor_id = '107');
+
+
+#Films rented by most profitable customer. You can use the customer table and payment table to find the most profitable customer ie the customer that has made the largest sum of payments
+
+SELECT title FROM film WHERE film_id IN (
+  SELECT film_id FROM inventory WHERE inventory_id IN (
+    SELECT inventory_id FROM rental WHERE customer_id IN (
+      SELECT customer_id FROM payment
+       GROUP BY customer_id
+         ORDER BY SUM(amount) DESC)));
+         
+#Customers who spent more than the average payments(this refers to the average of all amount spent per each customer).
+
+SELECT first_name, last_name FROM customer WHERE customer_id IN(
+ SELECT customer_id FROM payment 
+  GROUP BY customer_id
+   HAVING SUM(amount) > (SELECT (SUM(amount) / COUNT(DISTINCT(customer_id))) FROM payment));
